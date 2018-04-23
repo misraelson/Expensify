1de9c13b51d91af81f079c1ad9ea6a4b8fc41875
misraelson <misraelson@gmail.com>
2018-04-22 18:24:07 -0700

    section 4 lecture 28 and 29

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -20,12 +20,15 @@ var IndecisionApp = function (_React$Component) {
   _createClass(IndecisionApp, [{
     key: 'render',
     value: function render() {
+      var title = 'Indecision';
+      var subtitle = 'Put your life in hands computer';
+      var options = ['thing one', 'Thing two', 'Thing four', 'Thing 11'];
       return React.createElement(
         'div',
         null,
-        React.createElement(Header, null),
+        React.createElement(Header, { title: title, subtitle: subtitle }),
         React.createElement(Action, null),
-        React.createElement(Options, null),
+        React.createElement(Options, { options: options }),
         React.createElement(AddOption, null)
       );
     }
@@ -51,18 +54,19 @@ var Header = function (_React$Component2) {
   _createClass(Header, [{
     key: 'render',
     value: function render() {
+      console.log(this.props);
       return React.createElement(
         'dix',
         null,
         React.createElement(
           'h1',
           null,
-          'Indecision'
+          this.props.title
         ),
         React.createElement(
           'h2',
           null,
-          'Put your life in hands computer'
+          this.props.subtitle
         )
       );
     }
@@ -81,6 +85,11 @@ var Action = function (_React$Component3) {
   }
 
   _createClass(Action, [{
+    key: 'handlePick',
+    value: function handlePick() {
+      alert('handlePick');
+    }
+  }, {
     key: 'render',
     value: function render() {
       return React.createElement(
@@ -88,7 +97,7 @@ var Action = function (_React$Component3) {
         null,
         React.createElement(
           'button',
-          null,
+          { onClick: this.handlePick },
           'What should I do?'
         )
       );
@@ -108,17 +117,24 @@ var Options = function (_React$Component4) {
   }
 
   _createClass(Options, [{
+    key: 'handleRemoveAll',
+    value: function handleRemoveAll() {
+      alert('She Gone');
+    }
+  }, {
     key: 'render',
     value: function render() {
       return React.createElement(
         'div',
         null,
         React.createElement(
-          'p',
-          null,
-          'Options component goes here'
+          'button',
+          { onClick: this.handleRemoveAll },
+          'Remove All'
         ),
-        React.createElement(Option, null)
+        this.props.options.map(function (option) {
+          return React.createElement(Option, { key: option, optionText: option });
+        })
       );
     }
   }]);
@@ -141,7 +157,7 @@ var Option = function (_React$Component5) {
       return React.createElement(
         'div',
         null,
-        'OPTION Component Here'
+        this.props.optionText
       );
     }
   }]);
@@ -159,15 +175,31 @@ var AddOption = function (_React$Component6) {
   }
 
   _createClass(AddOption, [{
+    key: 'handleAddOption',
+    value: function handleAddOption(e) {
+      e.preventDefault();
+
+      var option = e.target.elements.option.value.trim();
+
+      if (option) {
+        alert(option);
+      }
+    }
+  }, {
     key: 'render',
     value: function render() {
       return React.createElement(
         'div',
         null,
         React.createElement(
-          'p',
-          null,
-          'AddOptions component goes here'
+          'form',
+          { onSubmit: this.handleAddOption },
+          React.createElement('input', { type: 'text', name: 'option' }),
+          React.createElement(
+            'button',
+            null,
+            'Add Option'
+          )
         )
       );
     }
@@ -182,3 +214,37 @@ var AddOption = function (_React$Component6) {
 
 
 ReactDOM.render(React.createElement(IndecisionApp, null), document.getElementById('app'));
+
+// Section 4 Lecture 18 : COMPONENT properties
+
+// We are going to pass data in everytime we intialize a component (such as the header title of each page), this data is called PROPS
+
+// We set up key value pairs. key can be anything we like
+// Inside out components we have access to THIS. THIS references the instance of that component we have rendered. In this case for Header title-"Test Value" is this.props
+// to pass it to our component, inside our h1 tag we add {jsx} a JavaScript expression with this.props.title
+
+// Inside the IndecisionApp component RENDER we defined a new const title to be passed via jsx to header in the return
+// We made a second const in IndecisionApp called subtitle. We gave header another prop called subtitle and set it equal to jsx {subtitle}
+// Inside header component we set {this.props.subtitle} and our subtitle shows up
+
+// created a new const inside IndecisionApp called options set it equal to some static strings
+// then created a new PROPS on the <Options /> in IndecisionApp component
+
+// inside the options component we access are options array this.props.options.map and use an arrow function and access the individual item in the array (option) => inside the <p> paragraph tag we want to set text and key. text being {option} inside p tags and key set to {option}
+
+// THEN we remove p tags and render an instance of <Option />
+// we then set up key equal to {option} and set up optionText={option}
+// then inside Option component we set up jsx rendering {this.props.optionText}
+// this gives us a ton of flexibility to change what option looks like. We can add text in front of {this.props.optionText} and it shows up for all the options
+
+// Section 4 Lecture 29: EVENTS AND METHODS
+
+// Apply jsx event handlers to class based components like Action
+
+// first we set up a new method before render() in Action component called handlePick() then inside our button we set up an onClick={this.handlePick}
+// back inside our new function we add an < alert('handlePick'); > that flashes a message to the screen when we click on that button
+
+// add our <form onSubmit={this.handleAddOption}> to the AddOption component, gave it an <input> with type and name and added <button>
+// then above inside the component (CLASS METHOD) we defined handleAddOption(e) that took in our event
+// defined a const variable called option and set it equal to e.target.elements.option.value.trim()
+// set the if(option) to return our alert(option) which will display the value of option in the flash message, so if option exists (is true) then alert(option)
--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 class IndecisionApp extends React.Component {
   render() {
+    const title = 'Indecision';
+    const subtitle = 'Put your life in hands computer';
+    const options = ['thing one', 'Thing two', 'Thing four', 'Thing 11']
     return (
       <div>
-        <Header />
+        <Header title={title} subtitle={subtitle} />
         <Action />
-        <Options />
+        <Options options={options} />
         <AddOption />
       </div>
     );
@@ -16,31 +19,41 @@ class IndecisionApp extends React.Component {
 // REact components all REQUIRE one method: render
 class Header extends React.Component {
   render() {
+   console.log(this.props)
     return (
       <dix>
-        <h1>Indecision</h1>
-        <h2>Put your life in hands computer</h2>
+        <h1>{this.props.title}</h1>
+        <h2>{this.props.subtitle}</h2>
       </dix>
     );
   }
 }
 
 class Action extends React.Component {
+  handlePick() {
+    alert('handlePick');
+  }
   render() {
     return (
       <div>
-        <button>What should I do?</button>
+        <button onClick={this.handlePick}>What should I do?</button>
       </div>
     );
   }
 }
 
 class Options extends React.Component {
+  handleRemoveAll() {
+    alert('She Gone');
+  }
   render() {
     return (
       <div>
-        <p>Options component goes here</p>
-        <Option />
+        <button onClick={this.handleRemoveAll}>Remove All</button>
+        {
+          this.props.options.map((option) => <Option key={option} optionText={option}/>)
+        }
+        {/* <Option /> */}
       </div>
     );
   }
@@ -50,17 +63,29 @@ class Option extends React.Component {
   render() {
     return (
       <div>
-        OPTION Component Here
+        {this.props.optionText}
       </div>
     );
   }
 }
 
 class AddOption extends React.Component {
+  handleAddOption(e) {
+    e.preventDefault();
+
+    const option = e.target.elements.option.value.trim();
+
+    if (option) {
+      alert(option);
+    }
+  }
   render() {
     return (
       <div>
-        <p>AddOptions component goes here</p>
+        <form onSubmit={this.handleAddOption}>
+          <input type="text" name="option" />
+          <button>Add Option</button>
+        </form>
       </div>
     );
   }
@@ -70,3 +95,37 @@ class AddOption extends React.Component {
 
 // to render we must use ReactDOM.render
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
+
+// Section 4 Lecture 18 : COMPONENT properties
+
+// We are going to pass data in everytime we intialize a component (such as the header title of each page), this data is called PROPS
+
+// We set up key value pairs. key can be anything we like
+// Inside out components we have access to THIS. THIS references the instance of that component we have rendered. In this case for Header title-"Test Value" is this.props
+// to pass it to our component, inside our h1 tag we add {jsx} a JavaScript expression with this.props.title
+
+// Inside the IndecisionApp component RENDER we defined a new const title to be passed via jsx to header in the return
+// We made a second const in IndecisionApp called subtitle. We gave header another prop called subtitle and set it equal to jsx {subtitle}
+// Inside header component we set {this.props.subtitle} and our subtitle shows up
+
+// created a new const inside IndecisionApp called options set it equal to some static strings
+// then created a new PROPS on the <Options /> in IndecisionApp component
+
+// inside the options component we access are options array this.props.options.map and use an arrow function and access the individual item in the array (option) => inside the <p> paragraph tag we want to set text and key. text being {option} inside p tags and key set to {option}
+
+// THEN we remove p tags and render an instance of <Option />
+// we then set up key equal to {option} and set up optionText={option}
+// then inside Option component we set up jsx rendering {this.props.optionText}
+// this gives us a ton of flexibility to change what option looks like. We can add text in front of {this.props.optionText} and it shows up for all the options
+
+// Section 4 Lecture 29: EVENTS AND METHODS
+
+// Apply jsx event handlers to class based components like Action
+
+// first we set up a new method before render() in Action component called handlePick() then inside our button we set up an onClick={this.handlePick}
+// back inside our new function we add an < alert('handlePick'); > that flashes a message to the screen when we click on that button
+
+// add our <form onSubmit={this.handleAddOption}> to the AddOption component, gave it an <input> with type and name and added <button>
+// then above inside the component (CLASS METHOD) we defined handleAddOption(e) that took in our event
+// defined a const variable called option and set it equal to e.target.elements.option.value.trim()
+// set the if(option) to return our alert(option) which will display the value of option in the flash message, so if option exists (is true) then alert(option)
