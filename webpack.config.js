cd8d2b9fc50f8c6a6a860366ce21bc6ddddf5bbf
misraelson <misraelson@gmail.com>
2018-04-24 14:12:28 -0700

    add yarn add webpack-dev-server@2.5.1 and config the to package.json

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,10 @@ module.exports = {
       exclude: /node_modules/
     }]
   },
-  devtool: 'cheap-module-eval-source-map'
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
+  }
 };
 
 // visit webpack.js.org for documentation
