2e2d0ec05cf0b5f71b4ed77b19ba7ac6f2f97e74
misraelson <misraelson@gmail.com>
2018-04-26 11:10:43 -0700

    added Switch and 404 error page component in app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -28,14 +28,21 @@ const HelpPage = () => (
   </div>
 );
 
+const NotFoundPage = () => (
+  <div>
+    404!
+  </div>
+);
+
 const routes = (
   <BrowserRouter>
-    <div>
+    <Switch>
       <Route path="/" component={ExpenseDashboardPage} exact={true} />
       <Route path="/create" component={AddExpensePage}/>
       <Route path="/edit" component={EditExpensePage}/>
       <Route path="/help" component={HelpPage}/>
-    </div>
+      <Route component={NotFoundPage} />
+    </Switch>
   </BrowserRouter>
 );
 
@@ -47,3 +54,9 @@ ReactDOM.render(routes, document.getElementById('app'));
 // just a single instance of <BrowserRouter> is required and we set up as many instances of <Route> as pages we have
 // set up exact for first route
 // config option historyApiFallback in webpack.config.js
+// Lecture 78
+// added switch for 404 pages: https://reacttraining.com/react-router/web/api/Switch
+// put Switch inside BrowserRouter => changed our <div> with routes inside to <Switch>
+// when ReactRouter sees switch it is going to move through route definitions in order and STOP when it sees a match
+// when it finds a route match, switch will stop, but our NotFoundPage will match when route is not defined
+// switch goes through one at a time and {NotFoundPage} is always found when there is no match
