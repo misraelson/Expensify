ced131420ce289e3ecd41c4bc547428f08bff5e4
misraelson <misraelson@gmail.com>
2018-04-25 17:34:12 -0700

    set up first routes

--- src/app.js
+++ src/app.js
@@ -10,10 +10,40 @@ const ExpenseDashboardPage = () => (
   </div>
 );
 
+const AddExpensePage = () => (
+  <div>
+    This Is My ADD EXPENSE Component
+  </div>
+);
+
+const EditExpensePage = () => (
+  <div>
+    This Is My EDIT EXPENSE Component
+  </div>
+);
+
+const HelpPage = () => (
+  <div>
+    This Is My HELP PAGE Component
+  </div>
+);
+
 const routes = (
   <BrowserRouter>
-    <Route path="/" component={ExpenseDashboardPage}/>
+    <div>
+      <Route path="/" component={ExpenseDashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage}/>
+      <Route path="/edit" component={EditExpensePage}/>
+      <Route path="/help" component={HelpPage}/>
+    </div>
   </BrowserRouter>
 );
 
 ReactDOM.render(routes, document.getElementById('app'));
+
+//
+// installed react router DOM
+// created router configuration
+// just a single instance of <BrowserRouter> is required and we set up as many instances of <Route> as pages we have
+// set up exact for first route
+// config option historyApiFallback in webpack.config.js
