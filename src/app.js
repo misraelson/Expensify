57b7ddd135cee55fc9c17f5c944ba0c7a5c19219
misraelson <misraelson@gmail.com>
2018-04-26 11:37:33 -0700

    added NavLink to our import statement of react-router-dom and set activeClassName to is-active on our NavLinks

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -30,19 +30,32 @@ const HelpPage = () => (
 
 const NotFoundPage = () => (
   <div>
-    404!
+    404! - <Link to="/">Home</Link>
   </div>
+);
+
+const Header = () => (
+  <header>
+    <h1>Expensify APP</h1>
+    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink> <br/>
+    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink> <br/>
+    <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink> <br/>
+    <NavLink to="/help" activeClassName="is-active">Get Help</NavLink> <br/>
+  </header>
 );
 
 const routes = (
   <BrowserRouter>
-    <Switch>
-      <Route path="/" component={ExpenseDashboardPage} exact={true} />
-      <Route path="/create" component={AddExpensePage}/>
-      <Route path="/edit" component={EditExpensePage}/>
-      <Route path="/help" component={HelpPage}/>
-      <Route component={NotFoundPage} />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component={ExpenseDashboardPage} exact={true} />
+        <Route path="/create" component={AddExpensePage}/>
+        <Route path="/edit" component={EditExpensePage}/>
+        <Route path="/help" component={HelpPage}/>
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
   </BrowserRouter>
 );
 
@@ -60,3 +73,13 @@ ReactDOM.render(routes, document.getElementById('app'));
 // when ReactRouter sees switch it is going to move through route definitions in order and STOP when it sees a match
 // when it finds a route match, switch will stop, but our NotFoundPage will match when route is not defined
 // switch goes through one at a time and {NotFoundPage} is always found when there is no match
+
+// Lecture 79 Learning to Link between pages
+// https://reacttraining.com/react-router/web/api/Link import Link into React Router DOM
+// in our NotFoundPage component added a Link: <Link to="/">Home</Link>
+// this does client side rendering, now we make call to ReactDom render() and renders "/"
+// BrowserRouter needs a single route element so we need a <div> then we dump an instance of <Header> inside the <div> below <BrowserRouter> and put all the <Switch> stuff below
+// now we see header everywhere
+// import NavLink https://reacttraining.com/react-router/web/api/NavLink
+// added activeClassName="is-active" to <NavLink> to allow us to style our links over in _base.scss partal we set font-weight to bold so that links are bold when user is on that page
+// Lecture 80
